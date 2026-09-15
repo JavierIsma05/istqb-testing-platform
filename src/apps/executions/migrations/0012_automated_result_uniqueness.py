7af9ac8202f6102566674a6dcfea4b3b57350793
Javier Ismael Aguilar Armijos <142855507+JavierIsma05@users.noreply.github.com>
2026-09-15 01:35:46 -0500

    fix(executions): reconcile duplicate automated results before constraint

--- src/apps/executions/migrations/0012_automated_result_uniqueness.py
+++ src/apps/executions/migrations/0012_automated_result_uniqueness.py
@@ -1,6 +1,19 @@
 from django.db import migrations, models
 
 
+def reconcile_duplicate_automated_results(apps, schema_editor):
+    AutomatedExecutionResult = apps.get_model('executions', 'AutomatedExecutionResult')
+    seen = set()
+    for row in AutomatedExecutionResult.objects.order_by('test_execution_id', 'validation_rule_id', '-id').values(
+        'id', 'test_execution_id', 'validation_rule_id'
+    ):
+        key = (row['test_execution_id'], row['validation_rule_id'])
+        if key in seen:
+            AutomatedExecutionResult.objects.filter(pk=row['id']).delete()
+        else:
+            seen.add(key)
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -8,6 +21,7 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.RunPython(reconcile_duplicate_automated_results, migrations.RunPython.noop),
         migrations.AddConstraint(
             model_name='automatedexecutionresult',
             constraint=models.UniqueConstraint(
