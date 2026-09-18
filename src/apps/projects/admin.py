19bc6ab6dfc0e6ff716808c6fe7f783a059b4bfb
Javier Ismael Aguilar Armijos <142855507+JavierIsma05@users.noreply.github.com>
2026-09-17 19:07:46 -0500

    fix: use safe project deletion in admin

--- src/apps/projects/admin.py
+++ src/apps/projects/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from .models import Project
+from .services import delete_project_and_related_data
 
 
 @admin.register(Project)
@@ -8,3 +9,8 @@ class ProjectAdmin(admin.ModelAdmin):
     list_display = ('code', 'name', 'status', 'start_date', 'end_date')
     list_filter = ('status',)
     search_fields = ('code', 'name')
+
+    def delete_queryset(self, request, queryset):
+        """Usa el mismo borrado seguro que la interfaz de proyectos."""
+        for project in queryset:
+            delete_project_and_related_data(project)
